771879ed48b16acba005d46a452be60b59b7c027
ged <winwisely99@gmail.com>
2019-10-21 13:05:00 +0200

    added screenhsot example

---
+++ example/go/screenshot/main.go
@@ -0,0 +1,46 @@
+// from: https://github.com/vova616/screenshot/blob/master/example/ss.go
+
+package main
+
+import (
+	"fmt"
+	"image/png"
+	"log"
+	"os"
+	"path/filepath"
+
+	"github.com/vova616/screenshot"
+)
+
+func main() {
+	img, err := screenshot.CaptureScreen()
+	if err != nil {
+		panic(err)
+	}
+
+	// make out dir
+	dir, err := os.Getwd()
+	if err != nil {
+		log.Fatal(err)
+	}
+	dirPath := filepath.Join(dir, "./bin/out")
+	fmt.Println("dirPath:", dirPath)
+	os.MkdirAll(dirPath, os.ModePerm)
+
+	filePath := filepath.Join(dirPath, "ss.png")
+	fmt.Println("filePath:", filePath)
+
+	//os.Exit(1)
+
+	// make file
+
+	f, err := os.Create(filePath)
+	if err != nil {
+		panic(err)
+	}
+	err = png.Encode(f, img)
+	if err != nil {
+		panic(err)
+	}
+	f.Close()
+}
