c8a7c1ef7935a45937b119de0b5bf68b9482a603
ged <winwisely99@gmail.com>
2019-11-10 12:52:25 +0100

    ci win fix

--- win/ci-bootstrap.ps1
+++ win/ci-bootstrap.ps1
@@ -2,14 +2,7 @@
 
 # Check to see if we are currently running "as Administrator"
 # See: https://github.com/mvijfschaft/dotfiles/blob/master/install.ps1
-if (!(Verify-Elevated)) {
-    $newProcess = new-object System.Diagnostics.ProcessStartInfo "PowerShell";
-    $newProcess.Arguments = $myInvocation.MyCommand.Definition;
-    $newProcess.Verb = "runas";
-    [System.Diagnostics.Process]::Start($newProcess);
- 
-    exit
- }
+
 
 # https://github.com/lukesampson/scoop
 Set-ExecutionPolicy RemoteSigned -scope CurrentUser
