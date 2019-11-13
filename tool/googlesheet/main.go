cc9f1a93fffdf47c6ed137bcdcaf856bcdfa791f
alizohrevand <alihooshyar1990@gmail.com>
2019-11-13 11:47:03 +0330

    add "url" flag to get url from CLI.

--- tool/googlesheet/main.go
+++ tool/googlesheet/main.go
@@ -1,21 +1,30 @@
 package main
 
 import (
+	"flag"
 	Services "github.com/winwisely99/bootstrap/tool/googlesheet/services"
 	"log"
 	"os"
+	"strconv"
+	TimeLib "time"
 )
 
 func main() {
 
 	// show line numbers
 	log.SetFlags(log.LstdFlags | log.Lshortfile)
+	Url := flag.String("url", "empty", "Url of googleSheet")
+	flag.Parse()
+	if *Url == "empty" {
+		log.Println("\nBad input.\nPlease enter valid url.")
+		return
 
+	}
 	log.Println("Starting Extracting Language Files from GoogleSheet - downloading csv approach..")
-
-	gsheetURL := "https://docs.google.com/spreadsheets/d/e/2PACX-1vQIhLNNfUKVjxMkMwdtTFnvuV8oN1H_OmgOWRCwHBkSfOo1fzA08LXDfcK4EA86fx18M4FeAIwOoBBR/pub?output=csv"
-
-	csvRelFilePath := "./outputs/gsheet.csv"
+	//gsheetURL := "https://docs.google.com/spreadsheets/d/e/2PACX-1vQIhLNNfUKVjxMkMwdtTFnvuV8oN1H_OmgOWRCwHBkSfOo1fzA08LXDfcK4EA86fx18M4FeAIwOoBBR/pub?output=csv"
+	gsheetURL := *Url
+	timeStamp := TimeLib.Now().Unix()
+	csvRelFilePath := "./outputs/" + strconv.FormatInt(timeStamp, 10) + "-gsheet.csv"
 	csvAbsFilePath, err := Services.GetAbsoluteFilePath(csvRelFilePath)
 
 	err = Services.Download(gsheetURL, csvAbsFilePath, 5000)
