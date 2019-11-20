2fb189faaea77783a4bf4a01bcd9c3f7f9b3ba5d
thevzurd <thevzurd@gmail.com>
2019-11-20 18:38:15 -0500

    Included MIME mapping

--- tool/googlesheet/services/file.go
+++ tool/googlesheet/services/file.go
@@ -19,6 +19,50 @@ import (
 	"github.com/tidwall/pretty"
 )
 
+// do not edit this mapping
+var mimeMap = map[string]string{
+	"video/3gpp":         "3gp",
+	"video/x-ms-asf":     "asf",
+	"video/x-msvideo":    "avi",
+	"image/bmp":          "bmp",
+	"application/msword": "doc",
+	"application/vnd.openxmlformats-officedocument.wordprocessingml.document": "docx",
+	"application/vnd.ms-excel": "xls",
+	"application/vnd.openxmlformats-officedocument.spreadsheetml.sheet": "xlsx",
+	"image/gif":                          "gif",
+	"application/x-gtar":                 "gtar",
+	"application/x-gzip":                 "gz",
+	"text/html":                          "html",
+	"image/jpeg":                         "jpg",
+	"audio/x-mpegurl":                    "m3u",
+	"audio/mp4a-latm":                    "m4a",
+	"video/vnd.mpegurl":                  "m4u",
+	"video/x-m4v":                        "m4v",
+	"video/quicktime":                    "mov",
+	"audio/x-mpeg":                       "mp3",
+	"video/mp4":                          "mp4",
+	"application/vnd.mpohun.certificate": "mpc",
+	"video/mpeg":                         "mpeg",
+	"audio/mpeg":                         "mpga",
+	"application/vnd.ms-outlook":         "msg",
+	"audio/ogg":                          "ogg",
+	"application/pdf":                    "pdf",
+	"image/png":                          "png",
+	"application/vnd.ms-powerpoint":      "ppt",
+	"application/vnd.openxmlformats-officedocument.presentationml.presentation": "pptx",
+	"audio/x-pn-realaudio":         "rmvb",
+	"application/rtf":              "rtf",
+	"application/x-tar":            "tar",
+	"application/x-compressed":     "tgz",
+	"text/plain":                   "txt",
+	"audio/x-wav":                  "wav",
+	"audio/x-ms-wma":               "wma",
+	"audio/x-ms-wmv":               "wmv",
+	"application/x-compress":       "z",
+	"application/x-zip-compressed": "zip",
+	"*/*":                          "",
+}
+
 func GetAbsoluteFilePath(relFilePath string, sheet string) (result string, err error) {
 	dir, err := os.Getwd()
 	if err != nil {
@@ -55,14 +99,19 @@ func Download(url string, filename string, timeout int64, sheet string, checkCSV
 	}
 	if checkCSV {
 		if resp.Header["Content-Type"][0] != "text/csv" && resp.Header["Content-Type"][0] != "text/csv; charset=utf-8" {
-			log.Printf(sheet, " : ", "The file downloaded has content type '%s', expected 'text/csv'.", resp.Header["Content-Type"])
-			return errors.New("Downloaded file didn't contain the expected content-type: 'text/csv'")
+			log.Printf(sheet, " : ", "The file downloaded has content type %s, expected text/csv", resp.Header["Content-Type"])
+			return errors.New("Downloaded file didn't contain the expected content-type: text/csv")
 		}
 	}
 	b, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
 		log.Println(sheet, " : ", "Cannot read Body of Response", err)
 		return errors.New("Cannot read Body of Response")
+	}
+
+	if !checkCSV {
+		filename = filename + "." + mimeMap[resp.Header["Content-Type"][0]]
+		log.Println(sheet, " : ", filename)
 	}
 
 	createFile(filename)
@@ -159,26 +208,6 @@ func createFile(path string) {
 	//log.Println("==> done creating file", path)
 }
 
-/*func createDirectory(dirName string) bool {
-	src, err := os.Stat(dirName)
-
-	if os.IsNotExist(err) {
-		// is recursive
-		errDir := os.MkdirAll(dirName, 0755)
-		if errDir != nil {
-			panic(err)
-		}
-		return true
-	}
-
-	if src.Mode().IsRegular() {
-		log.Println(dirName, "already exist as a file!")
-		return false
-	}
-
-	return false
-}*/
-
 func isError(err error) bool {
 	if err != nil {
 		log.Println(err.Error())
@@ -215,7 +244,7 @@ func WriteDataDumpFiles(csvFilePath string, jsonDirPath string, sheet string) er
 					m, _ := url.ParseQuery(u.RawQuery)
 					if m.Get("id") != "" {
 						link := fmt.Sprintf("https://drive.google.com/uc?authuser=0&id=%v&export=download", m["id"][0])
-						AbsFilePath, err := GetAbsoluteFilePath("./outputs/datadump/json/"+sheet+"/"+m["id"][0]+".png", sheet)
+						AbsFilePath, err := GetAbsoluteFilePath("./outputs/datadump/json/"+sheet+"/"+m["id"][0], sheet)
 						err = Download(link, AbsFilePath, 5000, sheet, false)
 						if err != nil {
 							log.Println(sheet, " : ", err)
